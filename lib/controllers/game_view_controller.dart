a4b0b91a491d9f7428b77382a71df155d2b2fa78
diak <diak@mesterlevel.lo>
2023-10-17 11:56:54 +0200

    controller bővitve lett

--- lib/controllers/game_view_controller.dart
+++ lib/controllers/game_view_controller.dart
@@ -1,4 +1,40 @@
 // játék logika
-import 'package:get/get.dart';
+import 'dart:math';
 
-class GameViewController extends GetxController {}
+import 'package:get/get.dart';
+import 'package:hangman/models/tipp.dart';
+
+class GameViewController extends  GetxController{
+List<String> szovegek=[
+  "alma piros",
+  "az ég kék",
+  "gurul a szekér",
+];
+
+String randomSzoveg="";
+String csillagosSzoveg="";
+
+void randomSzovegGeneralasa(){
+  int index= Random().nextInt(szovegek.length);
+  randomSzoveg=szovegek[index];
+  csillagosSzoveg ="";
+  for(int i=0;i<randomSzoveg.length;i++){
+    csillagosSzoveg+= "*";
+  }
+  update();
+}
+List<Tipp> tippek=[];
+bool nyertEaFelhasznalo(){
+  return csillagosSzoveg==randomSzoveg;
+
+}
+
+
+@override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    randomSzovegGeneralasa();
+    
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:hangman/views/game_view.dart';
 import 'package:hangman/views/home_view.dart';
 
-void main() {
-  runApp(GetMaterialApp(home: HomeView()));
+void main(){
+  runApp(GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:HomeView()
+  ));
 }
